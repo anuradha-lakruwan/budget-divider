f1398da0449aefc493aa1cf94f3451eafc40243e
Anuradha Lakruwan <128252764+anuradha-lakruwan@users.noreply.github.com>
2024-08-02 23:07:06 +0530

    updated the banners and fixed some issues

--- src/components/Expenses/Summary/ExpenseSummary.vue
+++ src/components/Expenses/Summary/ExpenseSummary.vue
@@ -36,7 +36,7 @@ export default {
       Object.keys(this.expenses).forEach((key) => {
         const expense = this.expenses[key];
 
-        const price = parseInt(expense.price, 10);
+        const price = parseFloat(expense.price, 10);
         expensesPerUser[expense.paidBy].paid += price;
 
         Object.keys(expensesPerUser).forEach((userId) => {
@@ -45,12 +45,6 @@ export default {
       });
 
       return expensesPerUser;
-    },
-    // To be removed ?
-    totalAmount() {
-      return Object
-        .keys(this.expenses)
-        .reduce((acc, key) => acc + parseInt(this.expenses[key].price, 10), 0);
     },
   },
 };
